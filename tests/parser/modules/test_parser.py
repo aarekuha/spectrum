ca86fe254b4ddd5671259e021765bda59f338d4c
Rekuha Alexander <aarekuha@gmail.com>
2022-10-08 03:36:33 +0500

    fix: decompose tests

--- tests/parser/modules/test_parser.py
+++ tests/parser/modules/test_parser.py
@@ -22,13 +22,32 @@ def make_html(links_count: int, page_title: str, parent_page: str = "") -> str:
     return html
 
 
-def test_get_links_parser(queue: Queue) -> None:
+def test_get_links_parser_empty(queue: Queue) -> None:
     start_url: str = "https://test.url/40"
     start_page_html: str = make_html(links_count=0, page_title="start page")
 
-    parser: Parser = Parser(queue=queue, start_url=start_url)
+    status_store: dict = {}
+    parser: Parser = Parser(queue=queue, start_url=start_url, status_store=status_store)
     links: set = parser._get_links(url=start_url, html=start_page_html)
     assert len(links) == 0
+
+    links_count: int = 5
+    child_page_html: str = make_html(
+        links_count=links_count,
+        page_title="Child page",
+        parent_page=start_url,
+    )
+    links = parser._get_links(url=start_url, html=child_page_html)
+    assert len(links) == links_count
+    with pytest.raises(AssertionError):
+        assert links == ""
+    for i in range(5):
+        assert start_url + str(i) in links
+
+def test_get_links_parser_containing(queue: Queue) -> None:
+    start_url: str = "https://test.url/40"
+    status_store: dict = {}
+    parser: Parser = Parser(queue=queue, start_url=start_url, status_store=status_store)
 
     links_count: int = 5
     child_page_html: str = make_html(
@@ -55,12 +74,22 @@ class MockResponse:
 
 
 @pytest.mark.asyncio
-async def test_check_site_avail(queue: Queue, mocker: MockerFixture) -> None:
-    parser: Parser = Parser(queue=queue, start_url="")
+async def test_check_site_avail_ok(queue: Queue, mocker: MockerFixture) -> None:
+    status_store: dict = {}
+    parser: Parser = Parser(queue=queue, start_url="", status_store=status_store)
     session: ClientSession = ClientSession()
 
     mocker.patch('aiohttp.ClientSession.get', return_value=MockResponse(status=200))
     assert await parser.check_site_avail(session=session) == True
+
+    await session.close()
+
+
+@pytest.mark.asyncio
+async def test_check_site_avail_failed(queue: Queue, mocker: MockerFixture) -> None:
+    status_store: dict = {}
+    parser: Parser = Parser(queue=queue, start_url="", status_store=status_store)
+    session: ClientSession = ClientSession()
 
     mocker.patch('aiohttp.ClientSession.get', return_value=MockResponse(status=404))
     assert await parser.check_site_avail(session=session) == False
